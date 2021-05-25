d800cbd77fa449f0c85461b1261fe7d9350098dd
GBH <gbh@GMBPro.local>
2021-05-25 16:03:26 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    title: "ms-build-slots",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
